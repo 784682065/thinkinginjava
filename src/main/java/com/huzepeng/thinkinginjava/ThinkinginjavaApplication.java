0aa725fac8eab21bcb72cacdd20dc5d03cb0e208
huzepeng <784682065@qq.com>
2018-12-26 09:31:34 +0800

    Initial commit

---
+++ src/main/java/com/huzepeng/thinkinginjava/ThinkinginjavaApplication.java
@@ -0,0 +1,14 @@
+package com.huzepeng.thinkinginjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinkinginjavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThinkinginjavaApplication.class, args);
+    }
+
+}
+
